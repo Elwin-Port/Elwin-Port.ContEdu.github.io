fe6d3a65dd22f6bc5f2410adcec5d4c932b6d157
Elwin-Port <102187516+Elwin-Port@users.noreply.github.com>
2025-04-25 16:14:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
+
   
     const rect1 = document.getElementById('rect1');
     const rect2 = document.getElementById('rect2');
+    const circle = document.getElementById('circle');
   
     // Modify the multiplier to control how fast they move apart
     const offset = Math.min(scrollY * 0.5, 500); // Limit max offset
+
   
     rect1.style.transform = `translateX(-${offset}px)`;
     rect2.style.transform = `translateX(${offset}px)`;
+    circle.style.transform = `translateY(-${offset}py)`;
+
+
   });
