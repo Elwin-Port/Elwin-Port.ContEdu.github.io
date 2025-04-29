9b8e27b08dcbaebadf430276772d4486869a8296
Elwin-Port <102187516+Elwin-Port@users.noreply.github.com>
2025-04-29 17:31:27 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
+    
 
   
     const rect1 = document.getElementById('rect1');
     const rect2 = document.getElementById('rect2');
     const circle = document.getElementById('circle');
+    
   
     // Modify the multiplier to control how fast they move apart
     const offset = Math.min(scrollY * 0.5, 500); // Limit max offset
@@ -15,4 +18,25 @@ window.addEventListener('scroll', () => {
     circle.style.transform = `translateY(-${offset}py)`;
 
 
+    
+
+
   });
+
+const circleTest = document.getElementById('circle');
+const circle = document.getElementById('circle');
+
+function updateReactiveCircle() {
+  const triggerRect = circle.getBoundingClientRect();
+  const reactiveRect = circleTest.getBoundingClientRect();
+
+  const triggerCenter = triggerRect.left + triggerRect.width /2;
+  const reactiveCenter = reactiveRect.left + reactiveRect.width /2;
+
+  const distance = Math.abs(triggerCenter - reactiveCenter);
+
+  const maxDistance = 500;
+  const scale = Math.maxs(1, 2 - distance / maxDistance);
+  
+  
+}
